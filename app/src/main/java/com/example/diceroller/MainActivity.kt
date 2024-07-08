39acbd7f8f6ac64bd30906734eb67556e85aa3d9
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-07-08 11:28:18 +0300

    Add second dice

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -13,6 +14,7 @@ import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -49,17 +51,28 @@ fun DiceRollerApp() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var result = remember { mutableStateOf(1) }
+    val dice1 = remember { mutableIntStateOf(1) }
+    val dice2 = remember { mutableIntStateOf(1) }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id = randomDiceImage(result.value)),
-            contentDescription = result.toString()
-        )
+        Row {
+            Image(
+                painter = painterResource(id = randomDiceImage(dice1.value)),
+                contentDescription = dice1.toString()
+            )
+
+            Image(
+                painter = painterResource(id = randomDiceImage(dice2.value)),
+                contentDescription = dice2.toString()
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result.value = (1..6).random() }) {
+        Button(onClick = {
+            dice1.value = (1..6).random()
+            dice2.value = (1..6).random()
+        }) {
             Text(stringResource(R.string.roll))
         }
     }
